bfa80289598099a55c6790bdcd23fb599ffc6633
MehulExplorer <mehul.r@techladd.com>
2023-10-28 19:40:21 +0530

    two courses added

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -2,6 +2,9 @@ import React from "react";
 import aptitude from "../images/aptitude.jpg";
 import design from "../images/design.png";
 import pcmb from "../images/pcb.jpg";
+import graphics from "../images/graphics.jpg";
+import drawing from "../images/drawing.png";
+
 const Courses = () => {
   return (
     <div>
@@ -148,6 +151,149 @@ const Courses = () => {
               </span>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="p-6 flex flex-wrap items-center justify-center">
+        <div className="flex-shrink-0 m-6 relative overflow-hidden bg-orange-300 rounded-lg max-w-xs shadow-lg">
+          <svg
+            className="absolute bottom-0 left-0 mb-8"
+            viewBox="0 0 375 283"
+            fill="none"
+            style={{ transform: "scale(1.5)", opacity: 0.1 }}
+          >
+            <rect
+              x="159.52"
+              y="175"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 159.52 175)"
+              fill="white"
+            />
+            <rect
+              y="107.48"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 0 107.48)"
+              fill="white"
+            />
+          </svg>
+          <div className="relative pt-10 px-10 flex items-center justify-center">
+            <div
+              className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+              style={{
+                background: "radial-gradient(black, transparent 60%)",
+                transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+                opacity: 0.2,
+              }}
+            ></div>
+            <img className="relative w-full" src={drawing} alt="" />
+          </div>
+          <div className="relative text-white px-6 pb-6 mt-6">
+            <span className="block opacity-75 mb-1">Arts and Drawing</span>
+            <div className="flex justify-between">
+              <span className="block font-semibold text-xl">Drawing </span>
+              <span className="block bg-white rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">
+                2hr class
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex-shrink-0 m-6 relative overflow-hidden bg-teal-500 rounded-lg max-w-xs shadow-lg">
+          <svg
+            className="absolute bottom-0 left-0 mb-8"
+            viewBox="0 0 375 283"
+            fill="none"
+            style={{ transform: "scale(1.5)", opacity: 0.1 }}
+          >
+            <rect
+              x="159.52"
+              y="175"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 159.52 175)"
+              fill="white"
+            />
+            <rect
+              y="107.48"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 0 107.48)"
+              fill="white"
+            />
+          </svg>
+          <div className="relative pt-10 px-10 flex items-center justify-center">
+            <div
+              className="block absolute w-48 h-48 bottom-0 left-0 mb-24 ml-3"
+              style={{
+                background: "radial-gradient(black, transparent 60%)",
+                transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+                opacity: 0.2,
+              }}
+            ></div>
+            <img className="relative w-full" src={graphics} alt="" />
+          </div>
+          <div className="relative text-white px-6 pb-6 mt-6">
+            <span className="block opacity-75 mb-1">Graphics</span>
+            <div className="flex justify-between">
+              <span className="block font-semibold text-xl">
+                Graphics Designing
+              </span>
+              <span className="block bg-white rounded-full text-teal-300 text-xs font-bold px-3 py-2 leading-none flex items-center">
+                2hr class
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex-shrink-0 m-6 relative overflow-hidden bg-purple-400 rounded-lg max-w-xs shadow-lg">
+          <svg
+            className="absolute bottom-0 left-0 mb-8"
+            viewBox="0 0 375 283"
+            fill="none"
+            style={{ transform: "scale(1.5)", opacity: 0.1 }}
+          >
+            <rect
+              x="159.52"
+              y="175"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 159.52 175)"
+              fill="white"
+            />
+            <rect
+              y="107.48"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 0 107.48)"
+              fill="white"
+            />
+          </svg>
+          {/* <div className="relative pt-10 px-10 flex items-center justify-center">
+            <div
+              className="block absolute w-48 h-48 bottom-0 left-0 mb-24 ml-3"
+              style={{
+                background: "radial-gradient(black, transparent 60%)",
+                transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+                opacity: 0.2,
+              }}
+            ></div>
+            <img className="relative w-full" src={aptitude} alt="" />
+          </div> */}
+          {/* <div className="relative text-white px-6 pb-6 mt-6">
+            <span className="block opacity-75 mb-1">Maths and Reasoning</span>
+            <div className="flex justify-between">
+              <span className="block font-semibold text-xl">Aptitude</span>
+              <span className="block bg-white rounded-full text-purple-400 text-xs font-bold px-3 py-2 leading-none flex items-center">
+                2hr class
+              </span>
+            </div>
+          </div> */}
         </div>
       </div>
       <div className="flex justify-end text-lg items-end mr-16">
